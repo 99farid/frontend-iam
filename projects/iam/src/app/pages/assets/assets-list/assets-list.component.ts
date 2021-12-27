c49e340fee990c81e9a3e73210da42a541153960
99farid <99.faridazhari@gmail.com>
2021-12-27 17:56:58 +0700

    membuat tampilan list dan insert asset

--- projects/iam/src/app/pages/assets/assets-list/assets-list.component.ts
+++ projects/iam/src/app/pages/assets/assets-list/assets-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FindAllResAssetsDto } from 'projects/core/src/app/dto/assets/find-all-res-assets-dto';
+import { Assets } from 'projects/core/src/app/model/assets';
+import { Files } from 'projects/core/src/app/model/files';
+import { AssetsService } from 'projects/core/src/app/services/assets/assets.service';
 
 @Component({
   selector: 'app-assets-list',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assets-list.component.css']
 })
 export class AssetsListComponent implements OnInit {
-
-  constructor() { }
+  listAsset : Assets[] = []
+  constructor(private assetsService : AssetsService) { }
 
   ngOnInit(): void {
+    this.assetsService.findAll().subscribe({next : result => {
+      let assetResult : FindAllResAssetsDto = result;
+      this.listAsset = assetResult.data
+     }
+    })
+  }
+  isDisplayAvail(data : Files) : boolean{
+    if(data){
+      return true;
+    } 
+    return false
   }
 
 }
