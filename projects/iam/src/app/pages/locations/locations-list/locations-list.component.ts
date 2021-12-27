94caf100c6671fa358d74f8e51438c4eb410c365
99farid <99.faridazhari@gmail.com>
2021-12-27 17:59:41 +0700

    menambahkan tampilan list locations

--- projects/iam/src/app/pages/locations/locations-list/locations-list.component.ts
+++ projects/iam/src/app/pages/locations/locations-list/locations-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FindAllResLocationsDto } from 'projects/core/src/app/dto/locations/find-all-res-locations-dto';
+import { Locations } from 'projects/core/src/app/model/locations';
+import { LocationsService } from 'projects/core/src/app/services/locations/locations.service';
 
 @Component({
   selector: 'app-locations-list',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locations-list.component.css']
 })
 export class LocationsListComponent implements OnInit {
-
-  constructor() { }
+  listLocations : Locations[] = []
+  constructor(private locationService : LocationsService) { }
 
   ngOnInit(): void {
+    this.locationService.findAll().subscribe({
+      next : result=>{
+        const locationResult : FindAllResLocationsDto = result
+        this.listLocations = locationResult.data 
+      }
+    })
   }
 
 }
