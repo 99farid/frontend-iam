d06aa209697a3c08502d374afd620e13301ef8b2
99farid <99.faridazhari@gmail.com>
2021-12-28 17:44:28 +0700

    membuat fitur update, delete, dan insert location

--- projects/core/src/app/services/locations/locations.service.ts
+++ projects/core/src/app/services/locations/locations.service.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DeleteResDataDto } from '../../dto/all-dto-global/delete-res-data-dto';
+import { InsertResDto } from '../../dto/all-dto-global/insert-res-dto';
+import { UpdateResDto } from '../../dto/all-dto-global/update-res-dto';
 import { FindAllResLocationsDto } from '../../dto/locations/find-all-res-locations-dto';
+import { FindByIdResLocationsDto } from '../../dto/locations/find-by-id-res-locations-dto';
+import { Locations } from '../../model/locations';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +17,21 @@ export class LocationsService {
 
   findAll() : Observable<FindAllResLocationsDto>{
     return this.http.get<FindAllResLocationsDto>('http://localhost:8080/locations')
+  }
+
+  findById(id : string | null) : Observable<FindByIdResLocationsDto>{
+    return this.http.get<FindByIdResLocationsDto>(`http://localhost:8080/locations/${id}`)
+  }
+
+  insert(data : Locations) : Observable<InsertResDto>{
+    return this.http.post<InsertResDto>(`http://localhost:8080/locations`,data)
+  }
+
+  update(data : Locations) : Observable<UpdateResDto>{
+    return this.http.put<UpdateResDto>(`http://localhost:8080/locations`,data)
+  }
+
+  delete(id : string | null) : Observable<DeleteResDataDto>{
+    return this.http.delete<DeleteResDataDto>(`http://localhost:8080/locations/${id}`)
   }
 }
--- projects/iam/src/app/pages/locations/locations-action/locations-action.component.ts
+++ projects/iam/src/app/pages/locations/locations-action/locations-action.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Select2OptionData } from 'ng-select2';
+import { FindAllFilterBySearchResCompaniesDto } from 'projects/core/src/app/dto/companies/find-all-filter-by-search-res-companies-dto';
+import { FindByIdResLocationsDto } from 'projects/core/src/app/dto/locations/find-by-id-res-locations-dto';
+import { Companies } from 'projects/core/src/app/model/companies';
+import { Locations } from 'projects/core/src/app/model/locations';
+import { AuthenticationService } from 'projects/core/src/app/services/authentication/authentication.service';
+import { LocationsService } from 'projects/core/src/app/services/locations/locations.service';
+import { Options } from 'select2';
 
 @Component({
   selector: 'app-locations-action',
@@ -6,10 +15,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locations-action.component.css']
 })
 export class LocationsActionComponent implements OnInit {
-
-  constructor() { }
+  optionCompany!: Options
+  dataInsert: Locations = new Locations()
+  companyInsert: Companies = new Companies()
+  dataUpdate: Locations = new Locations()
+  isDisabled: boolean = false
+  constructor(private authService: AuthenticationService, private locationService: LocationsService,
+    private activatedRoute: ActivatedRoute, private router : Router) { }
 
   ngOnInit(): void {
+    if (this.activatedRoute.snapshot.paramMap.get("id")) {
+      this.locationService.findById(this.activatedRoute.snapshot.paramMap.get("id")).subscribe(
+        result => {
+          const conditionResult: FindByIdResLocationsDto = result
+          this.dataUpdate = conditionResult.data
+          this.isDisabled = true
+
+          this.dataInsert.id = this.dataUpdate.id
+          this.dataInsert.code = this.dataUpdate.code
+          this.dataInsert.version = this.dataUpdate.version
+          this.dataInsert.createdBy = this.dataUpdate.createdBy
+          this.dataInsert.createdDate = this.dataUpdate.createdDate
+          
+        }
+      )
+    }
+
+    this.dataInsert.company = this.companyInsert
+
+    this.optionCompany = {
+      width: '100%',
+      ajax: {
+        headers: { Authorization: `Bearer ${this.authService.getToken()}` },
+        url: 'http://localhost:8080/companies/search/',
+        data: function (params) {
+          var query = {
+            query: params.term,
+          }
+          return query;
+        },
+        processResults: function (data) {
+          const result: FindAllFilterBySearchResCompaniesDto = data;
+          const select2Data: Select2OptionData[] = []
+          for (const company of result.data) {
+            select2Data.push(
+              {
+                id: company.id,
+                text: company.companyName
+              }
+            )
+          }
+          return {
+            results: select2Data
+          };
+        }
+
+      }
+    }
+  }
+
+  submit(): void {
+    if (this.dataInsert.id) {
+      this.locationService.update(this.dataInsert).subscribe({next :result=>{
+        this.router.navigateByUrl('/location-list')
+      }
+    })
+    } else {
+      this.locationService.insert(this.dataInsert).subscribe({next :result=>{
+        this.router.navigateByUrl('/location-list')
+      }
+    })
+    }
   }
 
 }
