c5a2e7c62bec7d0d123213a4cb6eaa4d5197371d
99farid <99.faridazhari@gmail.com>
2021-12-28 17:46:52 +0700

    membuat dto find by id

---
+++ projects/core/src/app/dto/invoices/find-by-id-res-invoices-dto.ts
@@ -0,0 +1,6 @@
+import { Invoices } from "../../model/invoices";
+
+export class FindByIdResInvoicesDto{
+    data!:Invoices;
+    msg!:string;
+}
---
+++ projects/core/src/app/dto/status-assets/find-by-id-res-status-assets-dto.ts
@@ -0,0 +1,6 @@
+import { StatusAssets } from "../../model/status-assets";
+
+export class FindByIdResStatusAssetsDto{
+    data!:StatusAssets;
+    msg!:string;
+}
