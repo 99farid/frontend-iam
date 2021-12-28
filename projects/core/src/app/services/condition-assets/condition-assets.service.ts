bc799d36acf93a977ba4b9d569411b36d47639bb
99farid <99.faridazhari@gmail.com>
2021-12-28 15:34:50 +0700

    membuat fitur update, insert, dan delete pada condition asset

--- projects/core/src/app/services/condition-assets/condition-assets.service.ts
+++ projects/core/src/app/services/condition-assets/condition-assets.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DeleteResDataDto } from '../../dto/all-dto-global/delete-res-data-dto';
+import { InsertResDto } from '../../dto/all-dto-global/insert-res-dto';
 import { FindAllResConditionAssetsDto } from '../../dto/condition-assets/find-all-res-condition-assets-dto';
+import { FindByIdResConditionAssetsDto } from '../../dto/condition-assets/find-by-id-res-condition-assets-dto';
+import { ConditionAssets } from '../../model/condition-assets';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +16,21 @@ export class ConditionAssetsService {
 
   findAll() : Observable<FindAllResConditionAssetsDto>{
     return this.http.get<FindAllResConditionAssetsDto>('http://localhost:8080/condition-assets')
+  }
+
+  findById(id:string | null) : Observable<FindByIdResConditionAssetsDto>{
+    return this.http.get<FindByIdResConditionAssetsDto>(`http://localhost:8080/condition-assets/${id}`)
+  }
+
+  insert(data : ConditionAssets) : Observable<InsertResDto>{
+    return this.http.post<InsertResDto>('http://localhost:8080/condition-assets', data)
+  }
+
+  update(data : ConditionAssets) : Observable<InsertResDto>{
+    return this.http.put<InsertResDto>('http://localhost:8080/condition-assets', data)
+  }
+
+  delete(id : string) : Observable<DeleteResDataDto>{
+    return this.http.delete<DeleteResDataDto>(`http://localhost:8080/condition-assets/${id}`)
   }
 }
