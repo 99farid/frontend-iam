e40bf1c3eb2f264b3a4d386a40e4cf6c8ed3b1c7
99farid <99.faridazhari@gmail.com>
2021-12-28 17:41:14 +0700

    membuat fitur insert, update, delate

--- projects/iam/src/app/pages/item-types/item-types-action/item-types-action.component.ts
+++ projects/iam/src/app/pages/item-types/item-types-action/item-types-action.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FindByIdResItemTypesDto } from 'projects/core/src/app/dto/item-types/find-by-id-res-item-types-dto';
+import { ItemTypes } from 'projects/core/src/app/model/item-types';
+import { ItemTypesService } from 'projects/core/src/app/services/item-types/item-types.service';
 
 @Component({
   selector: 'app-item-types-action',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-types-action.component.css']
 })
 export class ItemTypesActionComponent implements OnInit {
-
-  constructor() { }
+  isDisabled : boolean = false
+  dataInsert : ItemTypes = new ItemTypes()
+  dataUpdate : ItemTypes = new ItemTypes()
+   
+  constructor(private itemTypeService : ItemTypesService, private activatedRoute: ActivatedRoute,
+    private router : Router) { }
 
   ngOnInit(): void {
+    if(this.activatedRoute.snapshot.paramMap.get('id')){
+      this.itemTypeService.findById(this.activatedRoute.snapshot.paramMap.get('id')).subscribe(
+        result=> {
+          const typeResult : FindByIdResItemTypesDto = result
+          this.dataUpdate = typeResult.data
+          this.isDisabled = true
+
+          this.dataInsert.id = this.dataUpdate.id
+          this.dataInsert.code = this.dataUpdate.code
+          this.dataInsert.version = this.dataUpdate.version
+          this.dataInsert.createdBy = this.dataUpdate.createdBy
+          this.dataInsert.createdDate = this.dataUpdate.createdDate
+        }
+      )
+    }
+
   }
 
+  submit(): void {
+    if (this.dataInsert.id) {
+      this.itemTypeService.update(this.dataInsert).subscribe({next :result=>{
+        this.router.navigateByUrl('/item-types-list')
+      }
+    })
+    } else {
+      this.itemTypeService.insert(this.dataInsert).subscribe({next :result=>{
+        this.router.navigateByUrl('/item-types-list')
+      }
+    })
+    }
+  }
 }
--- projects/iam/src/app/pages/item-types/item-types-list/item-types-list.component.ts
+++ projects/iam/src/app/pages/item-types/item-types-list/item-types-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FindAllResConditionAssetsDto } from 'projects/core/src/app/dto/condition-assets/find-all-res-condition-assets-dto';
 import { FindAllResItemTypesDto } from 'projects/core/src/app/dto/item-types/find-all-res-item-types-dto';
 import { ItemTypes } from 'projects/core/src/app/model/item-types';
@@ -11,15 +12,31 @@ import { ItemTypesService } from 'projects/core/src/app/services/item-types/item
 })
 export class ItemTypesListComponent implements OnInit {
   listItemTypes : ItemTypes[] = []
-  constructor(private typesService : ItemTypesService) { }
+  constructor(private typesService : ItemTypesService, private router : Router) { }
 
   ngOnInit(): void {
     this.typesService.findAll().subscribe({
       next : result=>{
         let conditionResult : FindAllResItemTypesDto = result
         this.listItemTypes = conditionResult.data
+        
       }
     })
   }
 
+  clickCreate() : void {
+    this.router.navigateByUrl('/item-types-action/new')
+  }
+  
+  clickUpdate(id :string):void{
+    this.router.navigateByUrl(`/item-types-action/${id}`)
+  }
+
+  clickDelete(id : string) : void{
+    this.typesService.delete(id).subscribe(
+      result =>{
+        window.location.reload()
+      }
+    )
+  }
 }
