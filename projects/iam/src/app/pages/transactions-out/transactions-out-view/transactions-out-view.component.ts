22d2221e19adb8a2867ee462ea40f6a56d557cf0
savitaarsarini <desakayuputu5@gmail.com>
2021-12-30 13:12:47 +0700

    perbaikan pages profile dan role permission dan trasnout serta detailnya

--- projects/iam/src/app/pages/transactions-out/transactions-out-view/transactions-out-view.component.ts
+++ projects/iam/src/app/pages/transactions-out/transactions-out-view/transactions-out-view.component.ts
@@ -1,61 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FindByIdResDetailTransactionsOutDto } from 'projects/core/src/app/dto/detail-transactions-out/find-by-id-res-detail-transactions-out-dto';
+import { FindByIdResHeaderDto } from 'projects/core/src/app/dto/detail-transactions-out/find-by-id-res-header-dto';
+import { DetailTransactionsOut } from 'projects/core/src/app/model/detail-transactions-out';
+import { Files } from 'projects/core/src/app/model/files';
+import { DetailTransactionsOutService } from 'projects/core/src/app/services/detail-transactions-out/detail-transactions-out.service';
+import { TransactionsOutService } from 'projects/core/src/app/services/transactions-out/transactions-out.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-transactions-out-view',
   templateUrl: './transactions-out-view.component.html',
   styleUrls: ['./transactions-out-view.component.css']
 })
-export class TransactionsOutViewComponent implements OnInit {
+export class TransactionsOutViewComponent implements OnInit, OnDestroy {
 
-  listOrderDetail: OrderDetail[] = []
+  trxOutById!: FindByIdResHeaderDto
 
-  constructor(private router: Router) { }
+  listDetTrxOut: DetailTransactionsOut[] = []
+
+  id: string = String(this.activatedRoute.snapshot.paramMap.get('idHeader'))
+
+  private obs?: Subscription
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router,
+    private detailTransOutService: DetailTransactionsOutService) { }
 
   ngOnInit(): void {
-    const ordet1 = new OrderDetail()
-    ordet1.number = 1
-    ordet1.assetCode = "GLP1"
-    ordet1.display = "Laptop"
-    ordet1.itemName = "Laptop"
-    ordet1.company = "Lawencon"
-    this.listOrderDetail.push(ordet1)
-
-    const ordet2 = new OrderDetail()
-    ordet2.number = 2
-    ordet2.assetCode = "GPR1"
-    ordet2.display = "Printer"
-    ordet2.itemName = "Printer"
-    ordet2.company = "Linov"
-    this.listOrderDetail.push(ordet2)
-
-    const ordet3 = new OrderDetail()
-    ordet3.number = 3
-    ordet3.assetCode = "GTP1"
-    ordet3.display = "Printer"
-    ordet3.itemName = "Printer"
-    ordet3.company = "Linov"
-    this.listOrderDetail.push(ordet3)
-
-    const ordet4 = new OrderDetail()
-    ordet4.number = 4
-    ordet4.assetCode = "GHD1"
-    ordet4.display = "HDD 1TB"
-    ordet4.itemName = "HDD 1TB"
-    ordet4.company = "Lawencon"
-    this.listOrderDetail.push(ordet4)
+    if (this.id) {
+      this.obs = this.detailTransOutService.findByIdHeader(this.id)?.subscribe(result => {
+        this.trxOutById = result
+        this.listDetTrxOut = this.trxOutById.data
+      })
+    }
   }
+  
+  isDisplayAvail(data : Files) : boolean{
+    if(data){
+      return true;
+    } 
+    return false
+  }
+
 
   clickBack(): void {
-    this.router.navigateByUrl('/transactions-OUT-list')
+    this.router.navigateByUrl('/transactions-out-list')
   }
 
-}
+  ngOnDestroy(): void {
+    this.obs?.unsubscribe()
+  }
 
-class OrderDetail {
-  number?: number
-  assetCode?: string
-  display?: string
-  itemName?: string
-  company?: string
 }
