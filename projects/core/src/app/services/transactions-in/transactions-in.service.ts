e0aec5c81c9e42c3500c45ea4f472e697dd9489f
99farid <99.faridazhari@gmail.com>
2021-12-30 18:07:36 +0700

    membuat service transcation in

--- projects/core/src/app/services/transactions-in/transactions-in.service.ts
+++ projects/core/src/app/services/transactions-in/transactions-in.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InsertResDto } from '../../dto/all-dto-global/insert-res-dto';
+import { FindAllResTranscationsInDto } from '../../dto/transactions-in/find-all-res-transactions-in-dto';
+import { InsertReqTransactionInDto } from '../../dto/transactions-in/insert-req-transaction-in-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +11,10 @@ import { Injectable } from '@angular/core';
 export class TransactionsInService {
 
   constructor(private http : HttpClient) { }
-  
+  findAll() : Observable<FindAllResTranscationsInDto>{
+    return this.http.get<FindAllResTranscationsInDto>('http://localhost:8080/transactions-in/')
+  }
+  insert(data : InsertReqTransactionInDto) : Observable<InsertResDto>{
+    return this.http.post<InsertResDto>('http://localhost:8080/transactions-in/', data)
+  }
 }
